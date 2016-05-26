cebf995e49a64fae132bbd2d525ecd66a2ae96d3
Mateusz 'Haggus' Mrowiec <matt.mrowiec@gmail.com>
2016-05-26 16:17:39 +0200

    Serve javascript

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,15 @@ const index = {
     }
 };
 
-module.exports = index;
+const js = {
+    method: 'GET',
+    path: '/js/{file}',
+    config: {
+        auth: false,
+        handler: function(request, reply) {
+            reply.file('js/' + request.params.file);
+        }
+    }
+};
+
+module.exports = [index, js];
